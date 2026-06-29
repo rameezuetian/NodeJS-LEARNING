9f62be4bdf3ba067af2590c82d0599d980c3db64
rameezuetian <muhammadrameez262@gmail.com>
2026-06-29 09:30:13 +0500

    Handling URL's

--- 03-Https-Server/index.js
+++ 03-Https-Server/index.js
@@ -1,10 +1,29 @@
 const  http = require("http")
 const fs = require("fs")
+const url = require("url")
+
+
 
 const myServer = http.createServer((req , res)=>{
-    const log = `${Date.now()}: ${req.url} New Req Received \n`
+    if(req.url == "/favicon.ico") return res.end()
+    const log = `${Date.now()}: ${req.url} New Req Received \n`;
+    const myUrl = url.parse(req.url , true)
+    console.log(myUrl)
     fs.appendFile("log.txt" , log , (err , data)=>{
-        res.end("Hello From Server");
+        switch (myUrl.pathname) {
+            case "/":
+                res.end("HomePage")
+                break;
+            case "/about":
+                const username =  myUrl.query.myname
+                res.end(`h1 ${username}`)
+                break;
+            case "/contact":
+                res.end("Contact Page")
+                break;
+            default:
+                break;
+        }
     });
     
 });
