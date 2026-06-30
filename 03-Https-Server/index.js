77e3a7c28bd1b6967af39bf5369bc08da394598c
rameezuetian <muhammadrameez262@gmail.com>
2026-06-30 09:44:14 +0500

    URL Versionig

--- 03-Https-Server/index.js
+++ 03-Https-Server/index.js
@@ -1,34 +1,42 @@
-const  http = require("http")
-const fs = require("fs")
-const url = require("url")
+const express = require("express")
 
+const app = express();
 
+app.get('/' , (req , res)=>{
+    return res.send("Hello from Home Page")
+});
 
-const myServer = http.createServer((req , res)=>{
-    if(req.url == "/favicon.ico") return res.end()
-    const log = `${Date.now()}: ${req.url} New Req Received \n`;
-    const myUrl = url.parse(req.url , true)
-    console.log(myUrl)
-    fs.appendFile("log.txt" , log , (err , data)=>{
-        switch (myUrl.pathname) {
-            case "/":
-                res.end("HomePage")
-                break;
-            case "/about":
-                const username =  myUrl.query.myname
-                res.end(`h1 ${username}`)
-                break;
-            case "/contact":
-                res.end("Contact Page")
-                break;
-            default:
-                break;
-        }
-    });
-    
+app.get('/about'  , (req , res)=>{
+    res.send("HI from about Page")
 });
 
 
+// const myServer = http.createServer((req , res)=>{
+//     if(req.url == "/favicon.ico") return res.end()
+//     const log = `${Date.now()}: ${req.url} New Req Received \n`;
+//     const myUrl = url.parse(req.url , true)
+//     console.log(myUrl)
+//     fs.appendFile("log.txt" , log , (err , data)=>{
+//         switch (myUrl.pathname) {
+//             case "/":
+//                 res.end("HomePage")
+//                 break;
+//             case "/about":
+//                 const username =  myUrl.query.myname
+//                 res.end(`h1 ${username}`)
+//                 break;
+//             case "/contact":
+//                 res.end("Contact Page")
+//                 break;
+//             default:
+//                 break;
+//         }
+//     });
+    
+// });
+
+
+const myServer = http.createServer(app);
 
 
 myServer.listen(8000, ()=>console.log("Server Started!"))
