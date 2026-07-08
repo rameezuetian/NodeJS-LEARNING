5bdb63fa2f0a43beece4410bba6209f0681d92d4
rameezuetian <muhammadrameez262@gmail.com>
2026-07-08 14:53:47 +0500

    Blog and Comment Section

--- 09-Blog-Application/routes/blog.js
+++ 09-Blog-Application/routes/blog.js
@@ -1,6 +1,8 @@
 const {Router } = require("express")
 const router = Router();
 const path = require("path")
+const Blog = require('../models/blog')
+const Comment = require('../models/comment')
 
 
 
@@ -16,7 +18,24 @@ const storage = multer.diskStorage({
 })
 
 
+router.get("/:id" ,(req , res)=>{
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({blogId:req.params.id}).populate("createdBy");
+    return res.render("blog", {
+        user : req.user,
+        blog,
+        comments,
+    });
+});
 
+router.post('/comment/:blogId' , async (req , res)=>{
+    const comment = await Comment.create({
+        content:res.body.content,
+        blogId  : req.params.blogId,
+        createdBy : req.user._id
+    })
+
+})
 
 
 
